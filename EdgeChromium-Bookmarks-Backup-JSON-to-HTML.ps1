990b256203f2a6565522999f15e958c9edc043c2
Gunnar Haslinger <gh.github@hitco.at>
2019-11-20 09:06:21 +0100

    Add files via upload

---
+++ EdgeChromium-Bookmarks-Backup-JSON-to-HTML.ps1
@@ -0,0 +1,72 @@
+﻿# Read EdgeChromium Bookmarks (JSON File) and Export/Backup to HTML File
+# Gunnar Haslinger, 30.10.2019 - tested width EdgeChromium Beta 78
+
+# Path to EdgeChromium Bookmarks File and HTML Export
+$JSON_File_Path = "$env:localappdata\Microsoft\Edge Beta\User Data\Default\Bookmarks"
+if (!(Test-Path -Path $JSON_File_Path)) { throw "Source-File Path $JSON_File_Path does not exist!" }
+
+$HTML_File_Path = "$env:userprofile\backup"
+if (!(Test-Path -Path $HTML_File_Path)) { throw "Destination-Path $HTML_File_Path does not exist!" }
+$HTML_File_Path = "$HTML_File_Path\EdgeChromium-Bookmarks.backup.html"
+
+# Reference-Timestamp needed to convert Timestamps of JSON (Milliseconds / Ticks since LDAP / NT epoch 01.01.1601 00:00:00 UTC) to Unix-Timestamp (Epoch)
+$Date_LDAP_NT_EPOCH = Get-Date -Year 1601 -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0
+
+# ---- HTML Header ----
+$BookmarksHTML_Header = @'
+<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file.
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p>
+'@
+
+$BookmarksHTML_Header | Out-File -FilePath $HTML_File_Path -Force -Encoding utf8
+
+# ---- Enumerate Bookmarks Folders ----
+Function Get-BookmarkFolder {
+[cmdletbinding()] Param( [Parameter(Position=0,ValueFromPipeline=$True)] $Node )
+
+  Process {
+     foreach ($child in $node.children) { 
+       $DateAdded = [Decimal] $child.date_added
+           if ($DateAdded -gt 0) { 
+                # Timestamp Conversion: JSON-File uses Timestamp-Format "Ticks-Offset since LDAP/NT-Epoch" (reference Timestamp, Epoch since 1601 see above), HTML-File uses Unix-Timestamp (Epoch, since 1970)
+                $DateAdded = $Date_LDAP_NT_EPOCH.AddTicks($DateAdded * 10) # Get the JSON-Timestamp to a valid PowerShell DATE
+                # $DateAdded # Show Timestamp in Human-Readable-Format (Debugging-purposes only)
+                $DateAdded = $DateAdded | Get-Date -UFormat %s # Convert to Unix-Timestamp
+                $DateAdded = [int][double]::Parse($DateAdded) - 1 # Cut off the Milliseconds
+            }
+
+       $DateModified = [Decimal] $child.date_modified
+           if ($DateModified -gt 0) { # same conversion needed as above
+                $DateModified = $Date_LDAP_NT_EPOCH.AddTicks($DateModified * 10)
+                # $DateModified # Show Timestamp in Human-Readable-Format (Debugging-purposes only)
+                $DateModified = $DateModified | Get-Date -UFormat %s
+                $DateModified = [int][double]::Parse($DateModified) - 1
+            }
+
+       # Type folder or url?
+       if ($child.type -eq 'folder') 
+           { "        <DT><H3 FOLDED ADD_DATE=`"$($DateAdded)`" LAST_MODIFIED=`"$($DateModified)`">$($child.name)</H3>" | Out-File -FilePath $HTML_File_Path -Append -Force -Encoding utf8
+             "        <DL><p>" | Out-File -FilePath $HTML_File_Path -Append -Force -Encoding utf8
+             Get-BookmarkFolder $child # Recursive call in case of Folders / SubFolders
+             "        </DL><p>" | Out-File -FilePath $HTML_File_Path -Append -Force -Encoding utf8
+           }
+       else # Type not Folder => URL
+           { "        <DT><A HREF=`"$($child.url)`" ADD_DATE=`"$($DateAdded)`">$($child.name)</A>" | Out-File -FilePath $HTML_File_Path -Append -Encoding utf8
+           }
+     } # foreach
+  } # Process
+} # Function
+
+# ---- Convert the JSON Contens (recursive) ----
+$data = Get-content $JSON_File_Path -Encoding UTF8 | out-string | ConvertFrom-Json
+$sections = $data.roots.PSObject.Properties | select -ExpandProperty name
+ForEach ($entry in $sections) { $data.roots.$entry | Get-BookmarkFolder }
+
+# ---- HTML Footer ----
+'</DL>' | Out-File -FilePath $HTML_File_Path -Append -Force -Encoding utf8
---
+++ EdgeChromium-FirstRunExperience.ps1
@@ -0,0 +1,45 @@
+﻿# EdgeChromium - Disable First Run Experience (mark First Run Experiance FRU as Seen)
+# Gunnar Haslinger, 12.11.2019 - tested width EdgeChromium Beta 79
+
+# Define LocalAppData Folder
+$LocalAppData = $null; if ( $env:localappdata -ne $null) { $LocalAppData = $env:localappdata }
+
+# Config: Which Edge Branch? => "Edge Beta" or "Edge" or "Edge Dev"
+$EdgeBranch="Edge Beta"
+
+# Edge-Settings, starting with empty Hash - Import existing Settings if present
+$data = @{} 
+
+# Path to EdgeChromium Settings, create Edge-Settings-Folder if not present
+if (Test-Path $LocalAppData) {
+   $JSON_File_Path = "$LocalAppData\Microsoft\$EdgeBranch\User Data"; if (!(Test-Path $JSON_File_Path)) { "Creating ""$EdgeBranch"" Profile-Directory: ""$JSON_File_Path""" | Out-String; New-Item -ItemType Directory -Path $JSON_File_Path | Out-Null }
+   $JSON_File_Path = "$JSON_File_Path\Local State"
+} else { "ERROR: LocalAppData: ""$LocalAppData"" missing!" | Out-String; exit 1; }
+
+if   (!(Test-Path -Path $JSON_File_Path)) { "Edge-Settings-File ""$JSON_File_Path"" does not exist, starting empty." | Out-String }
+else { "Reading existing Settings-File: $JSON_File_Path" | Out-String 
+       $data = Get-content $JSON_File_Path -Encoding Default | ConvertFrom-Json
+     }
+
+if ($data.Count -gt 0) {
+	"# --- Existing Edge Settings --- " | Out-String
+	$data | Out-String
+	"-------------------------------- " | Out-String
+}
+
+if ($data.fre.has_user_seen_fre -eq "true") 
+     { "Edge First Run Experience (FRE) already done, no modification needed" | Out-String 
+     }
+else 
+     { "Edge First Run Experience (FRE) Setting is missing => Configuring: Set has_user_seen_fre=true" | Out-String
+
+       # Whole node "fre" is Missing? => Add
+       if ($data.fre -eq $null) { $data | Add-Member -Name fre -MemberType NoteProperty -Value @{has_user_seen_fre=$true} }
+
+       # Node "fre" is present, but SubEntry "has_user_seen_fre" is missing?
+       if ($data.fre.has_user_seen_fre -eq $null) { $data.fre | Add-Member -Name has_user_seen_fre -MemberType NoteProperty -Value $true; } 
+       else { $data.fre.has_user_seen_fre=$true } # SubEntry "has_user_seen_fre" is present but not true => set $true
+
+       "Writing Settings-File: $JSON_File_Path" | Out-String 
+       $data | ConvertTo-Json -Depth 99 | Out-File -FilePath $JSON_File_Path -Encoding default
+     }
